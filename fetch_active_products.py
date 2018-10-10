75c935aac124fc3a8301dcce7cb3f650789abda6
Cooper Ribb <cooperlimond@gmail.com>
2018-10-10 06:41:46 -0500

    Update fetch_active_products.py
    Cleaned up some dead/redundant code and updated `currentPrice` to fetch the `convertedCurrentPrice` instead (this should fix any previously incorrect currency-related issues).

--- fetch_active_products.py
+++ fetch_active_products.py
@@ -5,8 +5,7 @@ from gen_utils import get_trace_and_log
 import psycopg2
 import re
 from db_queries import prune_active
-from gen_utils import database_connection
-
+from gen_utils import database_connection, get_api_key, get_search_words, get_test_search_words
 
 active_product_ids = []
 active_product_nick = []
@@ -23,7 +22,6 @@ active_product_start = []
 active_product_end = []
 active_product_watch_count = []
 active_product_depth = []
-# active_product_avg = []
 depthCountStorage = []
 
 class SearchRequest(object):
@@ -32,7 +30,6 @@ class SearchRequest(object):
         self.search_body_pages = {
             'keywords': keyword,
             'itemFilter': [
-                # {'name': 'LocatedIn', 'value': 'US'},
                 # US only sellers -- can also limit by feedback score, business type, top-rated status, charity, etc.
                 {'name': 'MinPrice', 'value': '5', 'paramName': 'Currency', 'paramValue': 'USD'},
                 {'name': 'MaxPrice', 'value': '99999999', 'paramName': 'Currency', 'paramValue': 'USD'},
@@ -47,6 +44,8 @@ class SearchRequest(object):
             # can filter this to multiple different options as well (Best Offer, Most Watched, etc.)
             'sortOrder': 'PricePlusShippingLowest'
         }
+        self.api = Connection(siteid='EBAY-US', appid=self.api_key, config_file=None)
+
 
     def get_pages(self):
         """() -> dict
@@ -57,9 +56,8 @@ class SearchRequest(object):
         Returns an integer with the total number of pages.
         """
         try:
-            api = Connection(siteid='EBAY-US', appid=self.api_key, config_file=None)
-            api.execute('findItemsByKeywords', self.search_body_pages)
-            self.data = api.response.dict()
+            self.api.execute('findItemsByKeywords', self.search_body_pages)
+            self.data = self.api.response.dict()
             self.pages = int(self.data['paginationOutput']['totalPages'])
             return self.pages
         except ConnectionError as e:
@@ -70,18 +68,16 @@ class SearchRequest(object):
 
         Connects to the API,
         Iterates over each page in the previously established range of 1 -> the total number of pages,
-        Establish search_body_data parameters,
-        Execute a query to find items by their category and takes in predefined parameters search_body_data,
-        Return the data in dictionary form,
+        Establishes search_body_data parameters,
+        Executes a query to find items by their category and takes in predefined parameters search_body_data,
+        Returns the data in dictionary form,
         Iterates over each item in the returned data dictionary and appends the various data points to their respective lists,
         Prints the values.
         """
         try:
-            api = Connection(siteid='EBAY-US', appid=self.api_key, config_file=None)
             search_body_data = {
                 'keywords': self.keyword,
-                'itemFilter': [  # We also need to REMOVE duplicate listings in the future (?)
-                    # {'name': 'LocatedIn', 'value': 'US'},
+                'itemFilter': [
                     {'name': 'MinPrice', 'value': '5', 'paramName': 'Currency', 'paramValue': 'USD'},
                     {'name': 'MaxPrice', 'value': '99999999', 'paramName': 'Currency', 'paramValue': 'USD'},
                     {'name': 'ListingType', 'value': ['FixedPrice', 'StoreInventory', 'AuctionWithBIN']},
@@ -89,11 +85,10 @@ class SearchRequest(object):
                 'paginationInput':
                     {'entriesPerPage': '100',
                      'pageNumber': f'{page}'},
-                # can filter this to multiple different options as well
                 'sortOrder': 'PricePlusShippingLowest'}
 
-            api.execute('findItemsByKeywords', search_body_data)
-            self.data = api.response.dict()
+            self.api.execute('findItemsByKeywords', search_body_data)
+            self.data = self.api.response.dict()
             time.sleep(1)  # wait a second before continuing (be kind ^^)
         except Exception as e:
             get_trace_and_log(e)
@@ -190,7 +185,6 @@ class SearchRequest(object):
             # begin filtering magic :D
             if word.split(' ')[0] in {"Alpha", "Beta", "Unlimited", "Revised"}:
                 print(f'Chugging through...{page}/{self.pages} page(s)...')
-
                 depthCount = 0
                 for item in self.data['searchResult']['item']:
                     if not any(regex.findall(item['title']) for regex in set(outliers)):  # sets provide more iterating efficiency than lists.
@@ -207,10 +201,12 @@ class SearchRequest(object):
                             active_product_start.append(item['listingInfo']['startTime'])
                         except Exception as e:
                             active_product_start.append(0)
+                        # begin appending of lists
                         active_product_nick.append(word)
                         active_product_titles.append(item['title'])
                         active_product_ids.append(item['itemId'])
-                        active_product_prices.append(item['sellingStatus']['currentPrice']['value'])
+                        # active_product_prices.append(item['sellingStatus']['currentPrice']['value'])
+                        active_product_prices.append(item['sellingStatus']['convertedCurrentPrice']['value'])  # take the convertedCurrentPrice instead to get around having to convert CAD/AUD/etc. @ 10/10/2018
                         active_product_cat_names.append(item['primaryCategory']['categoryName'])
                         active_product_cat_ids.append(item['primaryCategory']['categoryId'])
                         active_product_img_thumb.append(item['galleryURL'])
@@ -219,7 +215,8 @@ class SearchRequest(object):
                         active_product_loc.append(item['location'])
                         active_product_end.append(item['listingInfo']['endTime'])
                         depthCount += 1
-
+                # if the page is 1 and the max number of pages is 1 then extend the depth to fill up the list, 
+                # otherwise proceed forwarrd
                 if self.pages == 1 and page == 1:
                     active_product_depth.extend(depthCount for i in range(depthCount))
                 elif self.pages > 1 and page == 1:
@@ -227,89 +224,44 @@ class SearchRequest(object):
                 else:
                     depthCountMulti = int(depthCountStorage[-1]) + depthCount
                     active_product_depth.extend(depthCountMulti for i in range(depthCountMulti))
-               
-                        # print('Title: {}'.format(item['title']))
-                        # print('Item ID: {}'.format(item['itemId']))
-                        # print('Price: ${}'.format(item['sellingStatus']['currentPrice']['value']))
-                        # print('Category Name: {}'.format(item['primaryCategory']['categoryName']))
-                        # print('Category ID: {}'.format(item['primaryCategory']['categoryId']))
-                        # print('Image Thumbnail: {}'.format(item['galleryURL']))
-                        # print('Image URL: {}'.format(item['viewItemURL']))
-                        # print('Listing type: {}'.format(item['listingInfo']['listingType']))
-                        # print('Product location: {}'.format(item['location']))
-                        # print('Condition: {}'.format(item['condition']['conditionDisplayName']))
-                        # print('Start time: {}'.format(item['listingInfo']['startTime']))
-                        # print('Watchers: {}'.format(item['listingInfo']['watchCount']))
-                        # print('Page: {}/{}'.format(int(page), int(self.data['paginationOutput']['totalPages'])))
-                        # print()
             else:
                 pass
         except KeyError as e:
             get_trace_and_log(e)
 
+def insert_active_products(cursor, zipFile):
+    """(db cursor, array) -> ()
 
-class DatabaseConnection(object):
-    def __init__(self):
+    Takes in a database connection (cursor) and an array of data. 
+    Proceeds to delete the old active values in the DB and then insert the contents of the passed in array."""
+    # begin delete (super raw for now)
+    cursor.execute("DELETE FROM active_products;")
+    # after delete, begin inserting fresh data
+    for a, b, c, d, e, f, g, h, i, j, k, l, m, n, o in zipFile:
         try:
-            self.connection = psycopg2.connect("dbname='EBAYMAGIC' user='postgres' host='localhost' password='magicarb123' port='5432'")
-            self.connection.autocommit = True
-            self.cursor = self.connection.cursor()
-            print("Successfully connected to database.")
+            cursor.execute("""INSERT INTO active_products(active_product_nick, active_product_titles, active_product_ids, active_product_prices, active_product_cat_names, active_product_cat_ids, active_product_img_thumb, active_product_img_url, active_product_lst_type, active_product_con, active_product_loc, active_product_watch_count, active_product_end, active_product_start, active_product_depth)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o,))  # MAKE SURE to leave the trailing comma (d-->,<--), this will NOT work otherwise.
+            print("Unique value inserted...")
         except Exception as e:
+            print("Unique value skipped...")
             get_trace_and_log(e)
-            print("Cannot connect to database.")
-
-    def insert_active_products(self, zipFile):
-        # drop table (drop data, but keep table structure in tact, before piping in new, fresh data) 
-        # we would show potentially stale results otherwise
-        # begin delete (super raw for now)
-        self.cursor.execute("DELETE FROM active_products;")
-        # after delete, begin inserting fresh data
-        for a, b, c, d, e, f, g, h, i, j, k, l, m, n, o in zipFile:
-            try:
-                self.cursor.execute("""INSERT INTO active_products(active_product_nick, active_product_titles, active_product_ids, active_product_prices, active_product_cat_names, active_product_cat_ids, active_product_img_thumb, active_product_img_url, active_product_lst_type, active_product_con, active_product_loc, active_product_watch_count, active_product_end, active_product_start, active_product_depth)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o,))  # MAKE SURE to leave the trailing comma (d-->,<--), this will NOT work otherwise.
-                print("Unique value inserted...")
-            except Exception as e:
-                print("Unique value skipped...")
-                get_trace_and_log(e)
-        print("Successfully piped database.")
+    print("Successfully piped database.")
 
 if __name__ == '__main__':
-    #TODO: could add back in time vaults?
-    words = ['Alpha Black Lotus', 'Alpha Mox Sapphire', 'Alpha Mox Jet', 'Alpha Mox Pearl', 'Alpha Mox Ruby', 'Alpha Mox Emerald', 'Alpha Timetwister', 'Alpha Ancestral Recall', 'Alpha Time Walk',
-                'Beta Black Lotus MTG', 'Beta Mox Sapphire', 'Beta Mox Jet', 'Beta Mox Pearl', 'Beta Mox Ruby', 'Beta Mox Emerald', 'Beta Timetwister', 'Beta Ancestral Recall', 'Beta Time Walk',
-                'Unlimited Black Lotus MTG', 'Unlimited Mox Sapphire', 'Unlimited Mox Jet', 'Unlimited Mox Pearl', 'Unlimited Mox Ruby', 'Unlimited Mox Emerald', 'Unlimited Timetwister', 'Unlimited Ancestral Recall', 'Unlimited Time Walk',
-                'Alpha Tundra MTG', 'Alpha Underground Sea MTG', 'Alpha Badlands MTG', 'Alpha Taiga MTG', 'Alpha Savannah MTG', 'Alpha Scrubland MTG', 'Alpha Volcanic Island MTG', 'Alpha Bayou MTG', 'Alpha Plateau MTG', 'Alpha Tropical Island MTG',
-                'Beta Tundra MTG', 'Beta Underground Sea MTG', 'Beta Badlands MTG', 'Beta Taiga MTG', 'Beta Savannah MTG', 'Beta Scrubland MTG', 'Beta Volcanic Island MTG', 'Beta Bayou MTG', 'Beta Plateau MTG', 'Beta Tropical Island MTG',
-                'Unlimited Tundra MTG', 'Unlimited Underground Sea MTG', 'Unlimited Badlands MTG', 'Unlimited Taiga MTG', 'Unlimited Savannah MTG', 'Unlimited Scrubland MTG', 'Unlimited Volcanic Island MTG', 'Unlimited Bayou MTG', 'Unlimited Plateau MTG', 'Unlimited Tropical Island MTG',
-                'Revised Tundra MTG', 'Revised Underground Sea MTG', 'Revised Badlands MTG', 'Revised Taiga MTG', 'Revised Savannah MTG', 'Revised Scrubland MTG', 'Revised Volcanic Island MTG', 'Revised Bayou MTG', 'Revised Plateau MTG', 'Revised Tropical Island MTG']
-                # 'Alpha Time Vault MTG', 'Beta Time Vault MTG', 'Unlimited Time Vault MTG']
-    # words = ['Beta Badlands MTG']
-    # 'Beta Tundra', 'Beta Underground Sea', 'Beta Badlands', 'Beta Taiga', 'Beta Savannah', 'Beta Scrubland', 'Beta Volcanic Island', 'Beta Bayou', 'Beta Plateau', 'Beta Tropical Island'
-    # 'Unlimited Tundra', 'Revised Underground Sea', 'Revised Badlands', 'Revised Taiga', 'Revised Savannah', 'Revised Scrubland', 'Revised Volcanic Island', 'Revised Bayou', 'Revised Plateau', 'Revised Tropical Island'
-    # 'Alpha Tundra', 'Alpha Underground Sea', 'Alpha Badlands', 'Alpha Taiga', 'Alpha Savannah', 'Alpha Scrubland', 'Alpha Volcanic Island', 'Alpha Bayou', 'Alpha Plateau', 'Alpha Tropical Island'
-    # 'CE Tundra', 'CE Underground Sea', 'CE Badlands', 'CE Taiga', 'CE Savannah', 'CE Scrubland', 'CE Volcanic Island', 'CE Bayou', 'CE Plateau', 'CE Tropical Island'
-    # 'ICE Tundra', 'ICE Underground Sea', 'ICE Badlands', 'ICE Taiga', 'ICE Savannah', 'ICE Scrubland', 'ICE Volcanic Island', 'ICE Bayou', 'ICE Plateau', 'ICE Tropical Island'
+    # pull in our api key, the list of words to iterate over, and begin zipping lists before piping the db
+    api_key = get_api_key()
+    words = get_search_words()
+    # comment out the above variable and use the one below when testing (includes 3 very common values)
+    # words = get_test_search_words()
     for word in words:
         print(f'Searching keyword: {word}')
-        x = SearchRequest('YOUR-API-KEY-HERE', word)
+        x = SearchRequest(api_key, word)
+        time.sleep(10)
         pages = x.get_pages() + 1
         for page in range(1, pages):
             x.fetch_active_data(page)
-    # print(f"{active_product_titles} {active_product_prices}")
-    # print(len(active_product_titles))
     # begin zipping of all arrays into one big-array, just before inserting into the database
     active_products = zip(active_product_nick, active_product_titles, active_product_ids, active_product_prices, active_product_cat_names, active_product_cat_ids, active_product_img_thumb, active_product_img_url, active_product_lst_type, active_product_con, active_product_loc, active_product_watch_count, active_product_end, active_product_start, active_product_depth)
     # begin piping data to the database)
-    database_connection = DatabaseConnection()
-    database_connection.insert_active_products(active_products)
-
-    # begin pruning of database (removing extreme outliers)
-    # cursor = generate_cursor()
-    # for value in words:
-    #     print(f'Pruning {value}....')
-    #     prune_active(value, cursor)
-    # print('----------------------------------')
-    # print('Succesfully pruned active_products')
-    # print('----------------------------------')
+    cursor = database_connection()
+    insert_active_products(cursor, active_products)
