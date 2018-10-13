496c84577253c7e98da699bb7c5569f22114e357
Cooper Ribb <cooperlimond@gmail.com>
2018-10-12 20:30:09 -0500

    Update gen_utils.py
    Added in the `csv_reader` and `get_timezones` functions into gen_utils.py.

--- gen_utils.py
+++ gen_utils.py
@@ -5,6 +5,9 @@ import requests
 import psycopg2
 import pandas as pd
 import configparser
+import csv
+from datetime import datetime, timedelta
+import pytz
 
 #TODO: assert checks if need be
 #TODO: memory profiling if need be
@@ -69,11 +72,22 @@ def get_search_words():
     """() -> list
 
     Reads a .txt file for search words and returns the corresponding values in list form."""
-    with open("search_words.txt") as txtFile:
-        words = []
-        for line in txtFile:
-            words.append(line)
-        return words
+    # the txt file format DOES NOT WORK, as it is inserting some sort of characters into the DB that we CANNOT remove. 
+    # The hard-coded array method is all that works so far.
+    # with open("search_words.txt") as txtFile:
+    #     words = []
+    #     for line in txtFile:
+    #         words.append(line)
+    #     return words
+    words = ['Alpha Black Lotus', 'Alpha Mox Sapphire', 'Alpha Mox Jet', 'Alpha Mox Pearl', 'Alpha Mox Ruby', 'Alpha Mox Emerald', 'Alpha Timetwister', 'Alpha Ancestral Recall', 'Alpha Time Walk',
+                'Beta Black Lotus MTG', 'Beta Mox Sapphire', 'Beta Mox Jet', 'Beta Mox Pearl', 'Beta Mox Ruby', 'Beta Mox Emerald', 'Beta Timetwister', 'Beta Ancestral Recall', 'Beta Time Walk',
+                'Unlimited Black Lotus MTG', 'Unlimited Mox Sapphire', 'Unlimited Mox Jet', 'Unlimited Mox Pearl', 'Unlimited Mox Ruby', 'Unlimited Mox Emerald', 'Unlimited Timetwister', 'Unlimited Ancestral Recall', 'Unlimited Time Walk',
+                'Alpha Tundra MTG', 'Alpha Underground Sea MTG', 'Alpha Badlands MTG', 'Alpha Taiga MTG', 'Alpha Savannah MTG', 'Alpha Scrubland MTG', 'Alpha Volcanic Island MTG', 'Alpha Bayou MTG', 'Alpha Plateau MTG', 'Alpha Tropical Island MTG',
+                'Beta Tundra MTG', 'Beta Underground Sea MTG', 'Beta Badlands MTG', 'Beta Taiga MTG', 'Beta Savannah MTG', 'Beta Scrubland MTG', 'Beta Volcanic Island MTG', 'Beta Bayou MTG', 'Beta Plateau MTG', 'Beta Tropical Island MTG',
+                'Unlimited Tundra MTG', 'Unlimited Underground Sea MTG', 'Unlimited Badlands MTG', 'Unlimited Taiga MTG', 'Unlimited Savannah MTG', 'Unlimited Scrubland MTG', 'Unlimited Volcanic Island MTG', 'Unlimited Bayou MTG', 'Unlimited Plateau MTG', 'Unlimited Tropical Island MTG',
+                'Revised Tundra MTG', 'Revised Underground Sea MTG', 'Revised Badlands MTG', 'Revised Taiga MTG', 'Revised Savannah MTG', 'Revised Scrubland MTG', 'Revised Volcanic Island MTG', 'Revised Bayou MTG', 'Revised Plateau MTG', 'Revised Tropical Island MTG',
+                'Alpha Time Vault MTG', 'Beta Time Vault MTG', 'Unlimited Time Vault MTG']
+    return words
 
 def get_test_search_words():
     """() -> list
@@ -81,3 +95,19 @@ def get_test_search_words():
     Returns a list of selected search words we can test against"""
     words = ['Unlimited Black Lotus MTG', 'Revised Tropical Island MTG', 'Beta Badlands MTG']
     return words
+
+def csv_reader(csv_path):
+    """(path: str, list: list) -> list
+
+    Takes in a designated path to the desired .csv and appends the rows to a global list."""
+    setList = []
+    with open(csv_path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            setList.append(row[0])
+        return setList
+
+def get_timezones(period_length):
+    CST = pytz.timezone('US/Central')
+    OLDEST_DATE = (datetime.now(CST) + timedelta(days=1)) - timedelta(days=period_length)
+    return OLDEST_DATE
